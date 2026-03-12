05f257c6916af40447bfdbd602ae69d39cca8af0
224ASTHA <sanchitabehera2005@gmail.com>
2026-03-12 16:34:57 +0530

    Subscription pipeline added

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -14,3 +14,8 @@ const subscriptionSchema = new Schema({
 
 export const Subscription = mongoose.model("Subscription",
     subscriptionSchema)
+
+// jitni baar v tum ek channel ko subscribe karoge utni baar ek document banega
+// the document consist of subscribers and channel itself
+// To count the number of subscriber => count the document having channel as that one
+// To count the number of channel the person is subscribing => We count the number of subs in the document
